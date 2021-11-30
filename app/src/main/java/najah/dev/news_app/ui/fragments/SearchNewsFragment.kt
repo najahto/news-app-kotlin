bb1dfb040bf5f55da239b090b0eb75b75f59f117
najahto <tahaomarnajah@gmail.com>
2021-11-30 21:16:37 +0100

    feat: search for news

--- app/src/main/java/najah/dev/news_app/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/najah/dev/news_app/ui/fragments/SearchNewsFragment.kt
@@ -1,17 +1,81 @@
 package najah.dev.news_app.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_search_news.*
+import kotlinx.coroutines.*
 import najah.dev.news_app.R
+import najah.dev.news_app.adapters.NewsAdapter
 import najah.dev.news_app.ui.NewsActivity
 import najah.dev.news_app.ui.NewsViewModel
+import najah.dev.news_app.utils.Constants
+import najah.dev.news_app.utils.Resource
 
 class SearchNewsFragment : Fragment(R.layout.fragment_search_news){
+
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+
+    val TAG = "SearchNewsFragment"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        var job: Job? = null
+        etSearch.addTextChangedListener { editable ->
+            job?.cancel()
+            job = MainScope().launch {
+                delay(Constants.SEARCH_NEWS_TIME_DELAY )
+                editable?.let {
+                    if(editable.toString().isNotEmpty()){
+                        viewModel.searchNews(editable.toString())
+                    }
+                }
+            }
+        }
+
+        viewModel.searchNews.observe(viewLifecycleOwner, Observer { response ->
+            when(response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error accrued: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
     }
+
+    private fun setupRecyclerView(){
+        newsAdapter = NewsAdapter()
+        rvSearchNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun hideProgressBar() {
+        paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
+    }
+
 }
--- app/src/main/java/najah/dev/news_app/utils/Constants.kt
+++ app/src/main/java/najah/dev/news_app/utils/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY= "ef087d6257c9460dad73b04850519084";
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
